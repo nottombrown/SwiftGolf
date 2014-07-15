ada284225115cc4c137c1ab02e72c39b08abc551
Tom Brown <tom@joingrouper.com>
2014-07-15 14:52:22 -0700

    Add basic tests

---
+++ SwiftGolf/golf.swift
@@ -0,0 +1,21 @@
+//
+//  golf.swift
+//  SwiftGolf
+//
+//  Created by Tom Brown on 7/15/14.
+//  Copyright (c) 2014 not. All rights reserved.
+//
+
+import Foundation
+
+class Golf {
+    
+    class func hole1() -> Int {
+        return 45
+    }
+    
+    class func hole2() {
+        
+    }
+    
+}
--- SwiftGolf/main.swift
+++ SwiftGolf/main.swift
@@ -8,5 +8,7 @@
 
 import Foundation
 
+
+
 println("Hello, World!")
 
